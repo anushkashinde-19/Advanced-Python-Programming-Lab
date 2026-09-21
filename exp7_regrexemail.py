0c3cbea7beb9d7566ffabd8d1d06e417ebc5ec5b
Anushka Shinde <anushkashinde312@gmail.com>
2026-09-21 09:31:52 +0530

    Add files via upload

---
+++ exp7_regrexemail.py
@@ -0,0 +1,17 @@
+import re
+
+def find_emails(text):
+    pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    
+    emails = re.findall(pattern, text)
+    
+    return emails
+
+
+if __name__ == "__main__":
+    text = input("Enter the text: ")
+    
+    result = find_emails(text)
+    
+    print("Email addresses found:", result)
+    
